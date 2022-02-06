77c85336b91a3080daf17faef56782827da247f4
Shiva Prakash Pendem <pendemsivaprakash@gmail.com>
2022-02-06 23:11:58 +0000

    my first react app

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <h1>Shower Thoughts</h1>
+      <div className="links">
+        <ul>
+          <li>Home</li>
+          <li>Shower Thoughts</li>
+          <li>Write a Thought</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
